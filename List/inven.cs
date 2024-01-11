6ae5128639d8209eb0d75f1dd86a81908b619427
chulwoo byun <bcw1992@naver.com>
2024-01-11 18:40:18 +0900

    test : 인벤연습..

---
+++ List/inven.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Item> list = new List<Item>();
+
+            list.Add(new Potion());
+            list.Add(new Sword());
+        }
+    }
+    public class Item
+    {
+
+    }
+    public class Potion : Item
+    {
+
+    }
+    public class Sword : Item
+    {
+
+    }
+}
